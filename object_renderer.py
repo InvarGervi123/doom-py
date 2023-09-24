6a58c4bc4cde41adc6b2e80cf6ca3cb2db2e8c57
InvarGervi123 <68227462+InvarGervi123@users.noreply.github.com>
2023-09-24 10:57:58 +0300

    sky with bug

--- object_renderer.py
+++ object_renderer.py
@@ -6,9 +6,17 @@ class ObjectRenderer:
         self.game = game 
         self.screen = game.screen
         self.wall_textures = self.load_wall_textures()
+        self.sky_image = self.get_texture('resources/textures/Just Tami.png', (WIDTH, HALF_HEIGHT))
+        self.sky_offset = 0
     
     def draw(self):
+        self.draw_background()
         self.rander_game_objects()
+
+    def draw_background(self):
+        self.sky_offset = (self.sky_offset + 4.0 * self.game.player.rel) % WIDTH
+        self.screen.blit(self.sky_image, (-self.sky_offset, 0))
+        self.screen.blit(self.sky_image, (-self.sky_offset + WIDTH, 0))
     
     def rander_game_objects(self):
         list_objects = self.game.raycasting.objects_to_render
