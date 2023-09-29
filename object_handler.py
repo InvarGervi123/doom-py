9199902ac6727b9f402fb1d57907b7624f323f85
InvarGervi123 <68227462+InvarGervi123@users.noreply.github.com>
2023-09-29 18:25:18 +0300

    intoriert save and test to fix

---
+++ object_handler.py
@@ -0,0 +1,20 @@
+from sprite_object import *
+
+
+class ObjectHandler:
+    def __init__(self, game):
+        self.game = game
+        self.sprite_list = []
+        self.static_sprite_path = 'resources/sprites/static_sprites/'
+        self.anim_sprite_path = 'resources/sprites/animated_sprites/'
+        add_sprite = self.add_sprite
+
+
+        add_sprite(SpriteObject(game))
+        add_sprite(AnimatedSprite(game))
+
+    def update(self):
+        [sprite.update() for sprite in self.sprite_list]
+
+    def add_sprite(self, sprite):
+        self.sprite_list.append(sprite)
--- sprite_object.py
+++ sprite_object.py
@@ -1,9 +1,11 @@
 import pygame as pg
 from settings import *
+import os 
+from collections import deque
 
 class SpriteObject:
     def __init__(self, game, path='resources/sprites/static_sprites/good.png',
-                  pos=(10.5,3.5), scale=1.0, shift=0.0):
+                  pos=(10.5,3.5), scale=0.7, shift=0.27):
         self.game = game
         self.player = game.player
         self.x, self.y = pos
@@ -22,8 +24,9 @@ class SpriteObject:
 
         image = pg.transform.scale(self.image, (proj_width, proj_height))
 
-        self.sprite_half_width = proj_width = proj_width // 2
-        pos = self.screen_x - self.sprite_half_width, HALF_HEIGHT - proj_height // 2
+        self.sprite_half_width = proj_width // 2
+        height_shift = proj_height * self.SPRITE_HEIGHT_SHIFT
+        pos = self.screen_x - self.sprite_half_width, HALF_HEIGHT - proj_height // 2 + height_shift
 
         self.game.raycasting.objects_to_render.append((self.norm_dist, image, pos))
 
@@ -42,8 +45,45 @@ class SpriteObject:
 
         self.dist = math.hypot(dx, dy)
         self.norm_dist = self.dist * math.cos(delta)
-        if self.IMAGE_HALF_WIDTH < self.screen_x < (WIDTH + self.IMAGE_HALF_WIDTH) and self.norm_dist > 0.5:
+        if -self.IMAGE_HALF_WIDTH < self.screen_x < (WIDTH + self.IMAGE_HALF_WIDTH) and self.norm_dist > 0.5:
             self.get_sprite_projection()
 
     def update(self):
         self.get_sprite()
+
+
+
+class AnimatedSprite(SpriteObject):
+    def __init__(self, game, path='resources/sprites/animated_sprites/green_light/0.png',
+                 pos=(11.5, 3.5), scale=0.8, shift=0.16, animation_time=120):
+        super().__init__(game, path, pos, scale, shift)
+        self.animation_time = animation_time
+        self.path = path.rsplit('/', 1)[0]
+        self.images = self.get_images(self.path)
+        self.animation_time_prev = pg.time.get_ticks()
+        self.animation_trigger = False
+
+    def update(self):
+        super().update()
+        self.check_animation_time()
+        self.animate(self.images)
+
+    def animate(self, images):
+        if self.animation_trigger:
+            images.rotate(-1)
+            self.image = images[0]
+
+    def check_animation_time(self):
+        self.animation_trigger = False
+        time_now = pg.time.get_ticks()
+        if time_now - self.animation_time_prev > self.animation_time:
+            self.animation_time_prev = time_now
+            self.aniamtion_trigger = True
+
+    def get_images(self, path):
+        images = deque()
+        for file_name in os.listdir(path):
+            if os.path.isfile(os.path.join(path, file_name)):
+                img = pg.image.load(path + '/' + file_name).convert_alpha()
+                images.append(img)
+        return images
