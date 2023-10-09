acf43f1d3d26ffce3175ce439173db993b26fdd7
InvarGervi123 <68227462+InvarGervi123@users.noreply.github.com>
2023-10-09 19:55:12 +0300

    WOW and fix by me bug and npc

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from sprite_object import *
 from object_handler import *
 from weapon import *
 from sound import *
+from pathfinding import *
 
 
 class Game:
@@ -18,6 +19,9 @@ class Game:
         self.screen = pg.display.set_mode(RES)
         self.clock = pg.time.Clock()
         self.delta_time = 1
+        self.global_trigger = False
+        self.global_event = pg.USEREVENT + 0
+        pg.time.set_timer(self.global_event, 40)
         self.new_game()
 
     def new_game(self):
@@ -30,6 +34,7 @@ class Game:
         self.object_handler = ObjectHandler(self)
         self.weapon = Weapon(self)
         self.sound = Sound(self)
+        self.pathfinding = PathFinding(self)
 
     def update(self):
         self.player.update()
@@ -50,10 +55,13 @@ class Game:
         #self.player.draw()
 
     def check_events(self):
+        self.global_trigger = False
         for event in pg.event.get():
             if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
                 pg.quit()
                 sys.exit()
+            elif event.type == self.global_event:
+                self.global_trigger = True
             self.player.single_fire_event(event)
         
     def run(self):
--- sprite_object.py
+++ sprite_object.py
@@ -82,7 +82,9 @@ class AnimatedSprite(SpriteObject):
 
     def get_images(self, path):
         images = deque()
-        for file_name in os.listdir(path):
+        for file_name in os.listdir(path):   
+            if file_name == 'desktop.ini':
+                continue         
             if os.path.isfile(os.path.join(path, file_name)):
                 img = pg.image.load(path + '/' + file_name).convert_alpha()
                 images.append(img)
